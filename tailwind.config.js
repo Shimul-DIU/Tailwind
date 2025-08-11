bbc89b17fd1b9f16a0357bb4bc8a5280fadd9acc
MD.Shimul <md.shimuldiu@gmail.com>
2025-08-11 15:11:33 +0600

    Initial study of tailwind list

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js,jsx,ts,tsx,vue}"
+  ],
+  theme: {
+    fontFamily:{
+      'shimul':['osward']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
+
