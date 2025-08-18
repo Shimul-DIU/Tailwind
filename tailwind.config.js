d75798bd6347094b12828b0c12da71bc717550b7
MD.Shimul <md.shimuldiu@gmail.com>
2025-08-19 02:33:07 +0600

    Initial design my first project

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,20 @@ module.exports = {
       fontFamily: {
         'shimul': ['osward', 'sans-serif'],
       },
+      container:{
+        center:true,
+        padding: {
+        DEFAULT: '1rem',  // default padding
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+      },
+      colors:{
+        'shimul':'rgba(121, 125, 252, 1)',
+      }
+
     },
   },
   plugins: [],
