bcd64ff77f50aad4c27c5b85e17daefa41a880c5
MD.Shimul <md.shimuldiu@gmail.com>
2025-08-19 16:50:32 +0600

    added footer and main section some section

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,16 +6,6 @@ module.exports = {
       fontFamily: {
         'shimul': ['osward', 'sans-serif'],
       },
-      container:{
-        center:true,
-        padding: {
-        DEFAULT: '1rem',  // default padding
-        sm: '2rem',
-        lg: '4rem',
-        xl: '5rem',
-        '2xl': '6rem',
-      },
-      },
       colors:{
         'shimul':'rgba(121, 125, 252, 1)',
       }
